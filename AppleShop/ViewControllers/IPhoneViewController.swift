fa732d9d30af87d6eeb640327594b11eecb4fb96
bondarbv <92814470+bondarbv@users.noreply.github.com>
2022-04-20 15:45:18 +0300

    Created UI on IphoneViewController and added OrderIphoneViewController

--- AppleShop/ViewControllers/IPhoneViewController.swift
+++ AppleShop/ViewControllers/IPhoneViewController.swift
@@ -9,14 +9,89 @@ import UIKit
 
 class IPhoneViewController: UIViewController {
     
-    let tabBarImage = UIImage(systemName: "iphone")
+    //MARK: -UI
+    let iphoneStackView: UIStackView = {
+        $0.axis = .vertical
+        $0.alignment = .fill
+        $0.distribution = .fill
+        $0.spacing = 20
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        return $0
+    }(UIStackView())
     
+    let iphoneLabel: UILabel = {
+        $0.text = "IPhone 13"
+        $0.font = UIFont.boldSystemFont(ofSize: 30)
+        $0.textAlignment = .center
+        return $0
+    }(UILabel())
+    
+    let iphoneSubtitle: UILabel = {
+        $0.text = "Oh. So. Pro."
+        $0.textAlignment = .center
+        return $0
+    }(UILabel())
+    
+    let priceLabel: UILabel = {
+        $0.text = "From 999$"
+        $0.textAlignment = .center
+        return $0
+    }(UILabel())
+    
+    let iphoneImageView: UIImageView = {
+        $0.image = UIImage(named: "test")
+        return $0
+    }(UIImageView())
+    
+    let orderButton: UIButton = {
+        $0.setTitle("Order", for: .normal)
+        $0.backgroundColor = .systemBlue
+        $0.layer.cornerRadius = 5
+        $0.addTarget(self, action: #selector(order), for: .touchUpInside)
+        return $0
+    }(UIButton())
+    
+    
+    //MARK: -ViewLifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemGray6
+        setupLayout()
         navigationItem.title = "IPhones"
         navigationController?.navigationBar.prefersLargeTitles = true
-        tabBarItem = UITabBarItem(title: "IPhones", image: tabBarImage, tag: 0)
+        tabBarItem = UITabBarItem(title: "IPhones", image: UIImage(systemName: "iphone"), tag: 0)
+    }
+    
+    //MARK: -Methods
+    
+    @objc private func order(sender: UIButton) {
+        if sender == orderButton {
+            let vc = OrderIPhoneViewController()
+            navigationController?.pushViewController(vc, animated: true)
+        }
+    }
+    
+    //MARK: -Layout
+    private func constraints() {
+        NSLayoutConstraint.activate([
+            iphoneStackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 30),
+            iphoneStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            iphoneStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            
+            iphoneImageView.widthAnchor.constraint(equalToConstant: 400),
+            iphoneImageView.heightAnchor.constraint(equalToConstant: 322)
+        ])
+    }
+
+    private func setupLayout() {
+        view.backgroundColor = .systemGray6
+        view.addSubview(iphoneStackView)
+        iphoneStackView.addArrangedSubview(iphoneLabel)
+        iphoneStackView.addArrangedSubview(iphoneSubtitle)
+        iphoneStackView.addArrangedSubview(iphoneImageView)
+        iphoneStackView.addArrangedSubview(priceLabel)
+        iphoneStackView.addArrangedSubview(orderButton)
+        constraints()
+        iphoneStackView.setCustomSpacing(0, after: iphoneLabel)
     }
 }
 
---
+++ AppleShop/ViewControllers/OrderIPhoneViewController.swift
@@ -0,0 +1,16 @@
+//
+//  OrderIPhoneViewController.swift
+//  AppleShop
+//
+//  Created by Bohdan on 20.04.2022.
+//
+
+import UIKit
+
+class OrderIPhoneViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemGray6
+    }
+}
